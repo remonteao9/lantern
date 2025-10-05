f0a95dd132022a0a4775b2b9b1fff9a854191405
gail <gradius827@gmail.com>
2025-10-05 15:46:33 +0900

    磁力実装

---
+++ Assets/Ingame/Script/Main/MagneticForce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagneticForce : MonoBehaviour
+{
+    public void Awake() {
+        GetComponent<SpriteRenderer>().enabled = false;
+        Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 point = new Vector2(wp.x, wp.y);
+
+        // クリック座標にある2Dコライダーを取得
+        Collider2D col = Physics2D.OverlapPoint(point);
+        if (col) {
+            var actor = col.gameObject.GetComponent<MNActor>();
+            if (actor) {
+                actor.Gravity();
+
+            }
+        }
+    }
+}
+
