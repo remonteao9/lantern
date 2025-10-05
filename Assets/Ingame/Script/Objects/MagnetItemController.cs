3837b68b1ea07a79c98076e4f281620a996705b1
remontea09 <seikaihaitinoseika@gmail.com>
2025-10-05 13:15:29 +0900

    MagnetItemの実装

---
+++ Assets/Ingame/Script/Objects/MagnetItemController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetItemController : MonoBehaviour
+{
+    private Camera cam;
+
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip SE;
+    public GameObject Selected { get; private set; }
+
+    void Awake() {
+        cam = cam ? cam : Camera.main;
+        Vector3 wp = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 point = new Vector2(wp.x, wp.y);
+
+        // クリック座標にある2Dコライダーを取得
+        Collider2D col = Physics2D.OverlapPoint(point);
+        if (col) {
+            if (col.GetComponent<TargetController>() != null) {
+                col.GetComponent<TargetController>().Magunet();
+            }
+            audioSource.clip = SE;
+            audioSource.Play();
+        }
+    }
+}
