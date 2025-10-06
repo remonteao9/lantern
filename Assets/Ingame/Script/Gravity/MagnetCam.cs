3b1c14e986f739f3f848b2683eafebe12b6c1665
gail <gradius827@gmail.com>
2025-10-06 23:13:23 +0900

    カメラ調整

---
+++ Assets/Ingame/Script/Gravity/MagnetCam.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MagnetCam : MonoBehaviour {
+    [SerializeField] private Transform iron;
+    [SerializeField] private Transform magnet;
+
+    private void Update() {
+        var newPosX = (iron.transform.position.x + magnet.transform.position.x) / 2;
+        var newPos = transform.position;
+        newPos.x = newPosX;
+        transform.position = newPos;
+    }
+}
